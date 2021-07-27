a13127c7786d4c81b5961e2b9770ea6ff57d6768
Sir StAwN <bmarava@hotmail.fr>
2021-07-27 23:44:56 +0200

    Initial commit

---
+++ numerologie.js
@@ -0,0 +1,25 @@
+//Exercice de numérologie @ https://www.callac-soft-college.fr/JavaScript/numerologie.pdf
+
+//DECLARER TABLEAU donné en énoncé ici
+
+function calc_prenom (prenoms)
+{
+	//utiliser tableau
+	//effectuer calcul
+	return res;
+}
+
+var prenom = window.prompt("Veuillez indiquer votre prénom");
+var disp = calc_prenom(prenom); // grâce au nombre retourné
+alert(disp);
+
+function calc_nom (noms)
+{
+	//utiliser tableau
+	//effectuer calcul
+	return res2;
+}
+
+var interet = window.prompt("Veuillez maintenant indiquer votre nom de famille"));
+var disp2 = calc_nom(nom); // grâce au nombre retourné
+alert(disp);
